108518da9d9289cd1e9ce677e850cedc0a1560b4
jeowsome <jeowsome15@gmail.com>
2023-04-12 16:26:15 +0800

    dyamic classes - usage of required keyword

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,20 +3,20 @@ import 'package:dice_app/styled_text.dart';
 
 const Alignment startAlignment = Alignment.topLeft;
 const Alignment endAlignment = Alignment.bottomRight;
-const List<Color> gradientList = [
-  Color.fromARGB(255, 26, 2, 80),
-  Color.fromARGB(255, 45, 7, 98)
-];
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({
+    super.key,
+    required this.colorList,
+  });
+  final List<Color> colorList;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: gradientList,
+          colors: colorList,
           begin: startAlignment,
           end: endAlignment,
         ),
